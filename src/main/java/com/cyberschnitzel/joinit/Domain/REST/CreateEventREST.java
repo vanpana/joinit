af2e1c3ca23eb5392f87db5abc2162b4b028ae16
vlentinlo <ioan.grozea@gmail.com>
2017-11-18 23:49:16 +0200

    added post method for event

--- src/main/java/com/cyberschnitzel/joinit/Domain/REST/CreateEventREST.java
+++ src/main/java/com/cyberschnitzel/joinit/Domain/REST/CreateEventREST.java
@@ -6,31 +6,23 @@ import com.cyberschnitzel.joinit.Domain.Response.ConfirmResponse;
 import com.cyberschnitzel.joinit.Repository.EventRepository;
 import com.cyberschnitzel.joinit.Repository.UserRepository;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("/createEvent")
 public class CreateEventREST {
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getMsg(@QueryParam("email") String email, @QueryParam("password") String password,
-                         @QueryParam("eventname") String eventname,
-                         @QueryParam("eventdescription") String eventdescription,
-                         @QueryParam("eventcategory") String eventcategory,
-                         @QueryParam("eventdate") String eventdate,
-                         @QueryParam("eventtime") String eventtime,
-                         @QueryParam("eventlocation") String eventlocation,
-                         @QueryParam("eventopen") int eventopen) {
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/{email}/{password}")
+    public String doPost(@QueryParam("email") String email, @QueryParam("password") String password,JsonObject event) {
         String filename = "/Users/vanpana/Documents/IntelliJ/joinit/data/joinit.db";
         Controller ctrl = new Controller(new UserRepository(filename), new EventRepository(filename));
 
-        if (ctrl.checkLogin(email, password)){
-            boolean open;
-            if (eventopen == 1) open = true;
-            else open = false;
-            ctrl.add(new Event(-1, eventname, eventdescription, eventcategory, eventdate, eventtime, eventlocation,
-                    ctrl.getUser(email).getId(), open));
+        if (ctrl.checkLogin(email, password)) {
+            Event eventClass = new Gson().fromJson(event, Event.class);
+            eventClass.setAdmin(ctrl.getUser(email).getId());
             return new Gson().toJson(new ConfirmResponse(true));
 
         }
